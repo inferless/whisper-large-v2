8aaa1afac7b5fe0688cb4acb62d7c6d0ba1b99bf
Nilesh Agarwal <nick.aggarwal@gmail.com>
2023-11-22 02:18:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ class InferlessPythonModel:
     def initialize(self):
         self.generator = pipeline(
             "automatic-speech-recognition",
-            model="openai/whisper-large-v2",
+            model="openai/whisper-large-v2", chunk_length_s=30,
             device_map="auto",
         )
 
     def infer(self, inputs):
         audio_url = inputs["audio_url"]
-        pipeline_output = self.generator(audio_url)
-        return {"transcribed_output": pipeline_output}
+        pipeline_output = self.generator(audio_url, )
+        return {"transcribed_output": pipeline_output["text"] }
 
     def finalize(self):
         self.processor = None
